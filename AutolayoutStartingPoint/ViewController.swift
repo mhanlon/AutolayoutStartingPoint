4b6cde3e6bfb703630638b5b79ad9ad3e0a6f785
Matt Hanlon <mhanlon@gmail.com>
2019-12-04 06:53:12 +0000

    Starting point UI.

--- AutolayoutStartingPoint/ViewController.swift
+++ AutolayoutStartingPoint/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func greetButtonTouched(_ sender: Any) {
+        self.greetingLabel.text = "Hello, " + (nameTextField.text)!
+    }
+    
 }
 
