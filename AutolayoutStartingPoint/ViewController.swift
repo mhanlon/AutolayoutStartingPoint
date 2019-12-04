41c03c269f1e198d2b2c723d1c881e378ecb51ee
Matt Hanlon <mhanlon@gmail.com>
2019-12-04 06:44:29 +0000

    Initial Commit

---
+++ AutolayoutStartingPoint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutolayoutStartingPoint
+//
+//  Created by Matthew Hanlon on 12/4/19.
+//  Copyright © 2019 The Code Hub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
